e4dab55d15902de3a9201f7963f57c605f79c645
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2024-09-05 17:23:59 +0100

    basic board class created

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,28 @@
 """Sudoku generator and solver, credits going to ... for the idea."""
+
+class Board:
+    def __init__(self, code=None):
+        
+        # create board representation from code
+        if code:
+            self.code = code
+
+            for row in range(9):
+                for column in range(9):
+                    self.board[row][column] = code[row + column]
+        else:
+            self.board = self.new_board()
+
+    # def get_code_from_board(self, board):
+
+    def new_board(self):
+        return [[0] * 9] * 9
+
+    def display_board(self):
+        for row in self.board:
+            print(row)
+
+    
+test = Board()
+
+test.display_board()
