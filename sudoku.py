5e19eb48856c29bbdeed26e5c794560f543fe5ac
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2024-09-16 15:00:35 +0100

    board fixes

--- sudoku.py
+++ sudoku.py
@@ -2,27 +2,49 @@
 
 class Board:
     def __init__(self, code=None):
-        
+        self.board = self.new_board()
+
         # create board representation from code
         if code:
             self.code = code
 
+            idx = 0
             for row in range(9):
                 for column in range(9):
-                    self.board[row][column] = code[row + column]
-        else:
-            self.board = self.new_board()
+                    self.board[row][column] = int(code[idx])
+                    idx += 1
+
 
     # def get_code_from_board(self, board):
 
     def new_board(self):
-        return [[0] * 9] * 9
+        return [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
 
     def display_board(self):
         for row in self.board:
             print(row)
 
+    def board_to_code(self):
+        code = ''
+        for row in self.board:
+            code += ''.join(map(str, row))
+
+        return code
+
+
     
 test = Board()
 
 test.display_board()
+
+print(test.board_to_code())
