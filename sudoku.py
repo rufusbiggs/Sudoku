372b1b09fe4d0bd536684cd129487c3925425586
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2024-09-16 16:52:28 +0100

    recusive solver added

--- sudoku.py
+++ sudoku.py
@@ -20,13 +20,13 @@ class Board:
     def new_board(self):
         return [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 7, 2, 0, 0, 1, 0, 3, 0],
+        [1, 8, 0, 0, 0, 6, 7, 2, 0],
+        [7, 0, 0, 2, 0, 0, 5, 6, 0],
+        [5, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 6, 9, 0, 0, 3, 0, 0, 7],
+        [0, 2, 4, 3, 0, 0, 0, 9, 1],
+        [0, 3, 0, 6, 0, 0, 4, 5, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
     ]
 
@@ -41,10 +41,67 @@ class Board:
 
         return code
 
+    def find_empty_cell(self):
+        for row in range(9):
+            for column in range(9):
+                if self.board[row][column] == 0:
+                    return (row, column)
+                
+        return False
+    
+    def try_solution(self, row, column, num):
+        if self.board[row][column] != 0:
+            return False
+        # check row
+        for val in range(9):
+            if self.board[val][column] == num:
+                return False
+        # check column
+        for val in range(9):
+            if self.board[row][val] == num:
+                return False    
+        # check 3x3 box
+        _3x3_row = row // 3
+        _3x3_col = column // 3
+        for i in range(3):
+            for j in range(3):
+                if self.board[i + (_3x3_row * 3)][j + (_3x3_col * 3)] == num:
+                    return False
+
+        return True
+    
+    def set_cell(self, row, col, num):
+        self.board[row][col] = num
+
+    def solve(self):
+        _empty_cell = self.find_empty_cell()
+
+        if not _empty_cell:
+            return True
+        else:
+            row, column = _empty_cell
+
+        for num in range(1, 10):
+            if self.try_solution(row, column, num):
+                self.board[row][column] = num
+
+                if self.solve():
+                    return self.board
+                
+            self.board[row][column] = 0
+
+        return False
+
 
     
 test = Board()
-
+print('Start')
+test.display_board()
+print('...')
+print('...')
+print('...')
+input("Hit enter to solve!")
+test.solve()
 test.display_board()
 
-print(test.board_to_code())
+
