3fa8b6996eaf87870f02e747fa29a4d671057be0
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2024-10-03 12:35:01 +0100

    start of api development, needs debugging in postman

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sudoku import Board
+
+app = Flask(__name__)
+
+@app.route('/generate', methods=['GET'])
+def generate_sudoku_board():
+    difficulty = int(request.args.get('difficulty', default = 1))
+
+    board_instance = Board()
+    board, solution = board_instance.generate_board_to_solve(difficulty)
+    return jsonify({
+        'board': board,
+        'solution': solution 
+    })
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
 
